55aa8991f6d15882371fa565fe45bbbfeb99faa4
wesley <wesleyfsmith@gmail.com>
2016-09-26 18:15:17 -0600

    Added more project descriptions.

--- chart.js
+++ chart.js
@@ -2,28 +2,28 @@
 var ctx = document.getElementById("myChart").getContext("2d");
 
 var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: [ "Linux", "Mobile Apps", "Programming", "Javascript", "C++", "Java", "MySQL", "Python", "NodeJS", "Meteor", "Angular", "React", "Teaching", "MongoDB"],
     datasets: [
         {
-            label: "My First dataset",
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            label: "Years of experience",
+            // backgroundColor: [
+            //     'rgba(255, 99, 132, 0.2)',
+            //     'rgba(54, 162, 235, 0.2)',
+            //     'rgba(255, 206, 86, 0.2)',
+            //     'rgba(75, 192, 192, 0.2)',
+            //     'rgba(153, 102, 255, 0.2)',
+            //     'rgba(255, 159, 64, 0.2)'
+            // ],
+            // borderColor: [
+            //     'rgba(255,99,132,1)',
+            //     'rgba(54, 162, 235, 1)',
+            //     'rgba(255, 206, 86, 1)',
+            //     'rgba(75, 192, 192, 1)',
+            //     'rgba(153, 102, 255, 1)',
+            //     'rgba(255, 159, 64, 1)'
+            // ],
             borderWidth: 1,
-            data: [65, 59, 80, 81, 56, 55, 40],
+            data: [ 12, 6, 12, 6, 4, 8, 6, 9, 4, 2, 3, 1, 7, 3],
         }
     ]
 };
@@ -32,13 +32,14 @@ var myBarChart = new Chart(ctx, {
     type: 'bar',
     data: data,
     options: {
-        scales: {
-            xAxes: [{
-                stacked: true
-            }],
-            yAxes: [{
-                stacked: true
-            }]
-        }
+        // scales: {
+        //     xAxes: [{
+        //         stacked: true
+        //     }],
+        //     yAxes: [{
+        //         stacked: false
+        //     }]
+        // },
+        stacked: false
     }
 });
